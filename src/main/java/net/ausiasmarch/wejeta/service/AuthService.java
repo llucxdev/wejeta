33c7c26c234a74753910cc7f8041cd5a834fa8a7
Rafael Aznar <rafaaznar@gmail.com>
2025-01-10 18:55:42 +0100

    de momento genera el token

--- src/main/java/net/ausiasmarch/wejeta/service/AuthService.java
+++ src/main/java/net/ausiasmarch/wejeta/service/AuthService.java
@@ -1,13 +1,54 @@
 package net.ausiasmarch.wejeta.service;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import javax.crypto.SecretKey;
+
 import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import net.ausiasmarch.wejeta.bean.LogindataBean;
 
 @Service
 public class AuthService {
 
+    public boolean checkLogin(LogindataBean oLogindataBean) {
+        if (oLogindataBean.getUser().equals("wejeta") && oLogindataBean.getPassword().equals("ausias")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private Map<String, String> getClaims() {
+        Map<String, String> claims = new HashMap<>();
+        claims.put("name", "wejeta");
+        claims.put("email", "wejeta@ausiasmarch.net");
+        return claims;
+    };
+
+    private SecretKey getSecretKey() {
+        String secretKey = "wejeta.ausias.54654654654654654bcvb54c6v5b46cv4b65c4v6b5c4v6b54c6seddffjrjrifjfdn";
+        return Keys.hmacShaKeyFor(secretKey.getBytes());
+    }
+
     public String getToken() {
         // generar el token jwt
+
         
-        return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhZG1pbiIsImlhdCI6MTUxNjIzOTAyMn";
+        return Jwts.builder()
+                .id(UUID.randomUUID().toString())
+                .claims(getClaims())
+                .subject("authentication")
+                .issuer("wejeta.ausiasmarch.net")
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + 6000000))                
+                .signWith(getSecretKey())                
+                .compact();
+
     }
 }
