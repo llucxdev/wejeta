2771c4aa1907429027f0603ec60a24c74497f0a5
Lluc Puertes Arrué <puertesarrue@gmail.com>
2025-01-20 21:21:12 +0100

    crud funcionando con login

--- src/main/java/net/ausiasmarch/wejeta/api/UsuarioController.java
+++ src/main/java/net/ausiasmarch/wejeta/api/UsuarioController.java
@@ -1,15 +1,24 @@
 package net.ausiasmarch.wejeta.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import net.ausiasmarch.wejeta.entity.TipousuarioEntity;
 import net.ausiasmarch.wejeta.entity.UsuarioEntity;
 import net.ausiasmarch.wejeta.service.UsuarioService;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @CrossOrigin(origins = "*", allowedHeaders = "*", maxAge = 3600)
 @RestController
@@ -19,6 +28,12 @@ public class UsuarioController {
     @Autowired
     UsuarioService oUsuarioService;
 
+    @GetMapping("")
+    public ResponseEntity<Page<UsuarioEntity>> getPage(
+            Pageable oPageable) {
+        return new ResponseEntity<Page<UsuarioEntity>>(oUsuarioService.getPage(oPageable), HttpStatus.OK);
+    }
+
     @GetMapping("/restricted")
     public ResponseEntity<String> restricted() {
         return ResponseEntity.ok("\"" + oUsuarioService.RestrictedArea() + "\"");
@@ -26,8 +41,22 @@ public class UsuarioController {
 
     @GetMapping("/{id}")
     public ResponseEntity<UsuarioEntity> getUsuarioById(@PathVariable(value = "id") Long id) {
-        return ResponseEntity.ok(oUsuarioService.get(id));
+        return new ResponseEntity<UsuarioEntity>(oUsuarioService.get(id), HttpStatus.OK);
     }
 
+    @PostMapping("")
+    public ResponseEntity<UsuarioEntity> create(@RequestBody UsuarioEntity oUsuarioEntity) {
+        return new ResponseEntity<UsuarioEntity>(oUsuarioService.create(oUsuarioEntity), HttpStatus.OK);
+    }
+
+    @PutMapping("")
+    public ResponseEntity<UsuarioEntity> update(@RequestBody UsuarioEntity oUsuarioEntity) {
+        return new ResponseEntity<UsuarioEntity>(oUsuarioService.update(oUsuarioEntity), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Long> delete(@PathVariable Long id) {
+        return new ResponseEntity<Long>(oUsuarioService.delete(id), HttpStatus.OK);
+    }
 
 }
--- src/main/java/net/ausiasmarch/wejeta/service/UsuarioService.java
+++ src/main/java/net/ausiasmarch/wejeta/service/UsuarioService.java
@@ -1,12 +1,16 @@
 package net.ausiasmarch.wejeta.service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
 import net.ausiasmarch.wejeta.entity.UsuarioEntity;
-import net.ausiasmarch.wejeta.exception.UnauthorizedAccessException;
+import net.ausiasmarch.wejeta.exception.ResourceNotFoundException;
+import net.ausiasmarch.wejeta.repository.TipousuarioRepository;
 import net.ausiasmarch.wejeta.repository.UsuarioRepository;
+import net.ausiasmarch.wejeta.entity.TipousuarioEntity;
 
 @Service
 @AllArgsConstructor
@@ -18,6 +22,8 @@ public class UsuarioService {
 
     AuthService oAuthService;
 
+    TipousuarioRepository oTipousuarioRepository;
+
     public String RestrictedArea() {
         if (oHttpServletRequest.getAttribute("email") == null) {
             return "No tienes permisos para acceder a esta zona";
@@ -25,13 +31,51 @@ public class UsuarioService {
             return "Bienvenido a la zona restringida";
         }
     }
-    
-    public UsuarioEntity get(Long id){
-        if (oAuthService.isContableWithItsOwnData(id) || oAuthService.isAdmin()) {
-            return oUsuarioRepository.findById(id).get();
-        } else {
-            throw new UnauthorizedAccessException("No tienes permisos para acceder a esta zona");
-        }        
+
+    public UsuarioEntity get(Long id) {
+        return oUsuarioRepository.findById(id).get();
     }
 
+    public Page<UsuarioEntity> getPage(Pageable oPageable) {
+        return oUsuarioRepository.findAll(oPageable);
+    }
+
+    public UsuarioEntity create(UsuarioEntity oUsuarioEntity) {
+
+        TipousuarioEntity oTipousuarioEntity = oTipousuarioRepository.findById(oUsuarioEntity.getTipousuario().getId())
+                .orElseThrow(
+                        () -> new ResourceNotFoundException("No se encuentra el tipo de usuario"));
+
+        oUsuarioEntity.setTipousuario(oTipousuarioEntity);
+        return oUsuarioRepository.save(oUsuarioEntity);
+    }
+
+    public UsuarioEntity update(UsuarioEntity oUsuarioEntity) {
+        UsuarioEntity oUsuarioEntityFromDatabase = oUsuarioRepository.findById(oUsuarioEntity.getId()).get();
+        if (oUsuarioEntity.getNombre() != null) {
+            oUsuarioEntityFromDatabase.setNombre(oUsuarioEntity.getNombre());
+        }
+        if (oUsuarioEntity.getApellido1() != null) {
+            oUsuarioEntityFromDatabase.setApellido1(oUsuarioEntity.getApellido1());
+        }
+        if (oUsuarioEntity.getApellido2() != null) {
+            oUsuarioEntityFromDatabase.setApellido2(oUsuarioEntity.getApellido2());
+        }
+        if (oUsuarioEntity.getEmail() != null) {
+            oUsuarioEntityFromDatabase.setEmail(oUsuarioEntity.getEmail());
+        }
+        if (oUsuarioEntity.getPassword() != null) {
+            oUsuarioEntityFromDatabase.setPassword(oUsuarioEntity.getPassword());
+        }
+        if (oUsuarioEntity.getTipousuario() != null) {
+            oUsuarioEntityFromDatabase
+                    .setTipousuario(oTipousuarioRepository.findById(oUsuarioEntity.getTipousuario().getId()).get());
+        }
+        return oUsuarioRepository.save(oUsuarioEntityFromDatabase);
+    }
+
+    public Long delete(Long id) {
+        oUsuarioRepository.deleteById(id);
+        return 1L;
+    }
 }
